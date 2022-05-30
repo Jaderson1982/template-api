306ed399352715515a3e27728d414ca6499a515a
Jaderson Torquato <jaderson.wt@gmail.com>
2022-05-30 18:27:17 -0300

    criacao da classe modelo

---
+++ src/main/java/br/com/jad/api/model/UsuarioModel.java
@@ -0,0 +1,75 @@
+package br.com.jad.api.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_usuario")
+public class UsuarioModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private Integer idade;
+
+    @Column(nullable = false)
+    private String sexo;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Deprecated
+    public UsuarioModel() {
+    }
+
+    public UsuarioModel(String nome, Integer idade, String sexo, String cpf) {
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.cpf = cpf;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+   public String getNome() {
+        return nome;
+   }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
